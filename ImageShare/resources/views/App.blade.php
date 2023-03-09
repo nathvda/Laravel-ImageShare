b0b05d47560a21372a3fcf7d71643f90d2695bff
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-03-09 09:43:33 +0100

    Initial Commit

---
+++ ImageShare/resources/views/App.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @vite('resources/css/app.css')
+
+    </head>
+    <body class="antialiased flex h-full bg-slate-200 flex-col justify-center content-center gap-8">
+    <main>
+    <section class="bg-white bottom-9 border-r">main content</section>
+    <section class="bg-white">??</section>
+    </main>
+    </body>
+</html>
