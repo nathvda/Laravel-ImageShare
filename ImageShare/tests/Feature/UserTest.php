ff377425ab9f671e52dac2df4d6e8f2682423f57
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-04-14 13:47:05 +0200

    more tests

--- ImageShare/tests/Feature/UserTest.php
+++ ImageShare/tests/Feature/UserTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Auth\User;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -12,22 +13,8 @@ class UserTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_create_user(): void
+    public function test_create(): void
     {
-        $user = User::create([
-            'lastname' => 'yop',
-            'firstname' => 'firstname',
-            'username' => 'username',
-            'avatar' => 'idk.png',
-            'email' => 'jkldf@mail.com',
-            'password' => bcrypt('password')
-        ]);
-
-        $this->assertArrayHasKey('lastname',$user);
-        $this->assertArrayHasKey('firstname',$user);
-        $this->assertArrayHasKey('username',$user);
-        $this->assertArrayHasKey('avatar',$user);
-        $this->assertArrayHasKey('email',$user);
-        $this->assertArrayHasKey('password',$user);
+        
     }
 }
